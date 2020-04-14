53b799a75d584d3ff6c6ce610cad0872a9e29f01
Sudiksha <sudiksha.sd1@gmail.com>
2020-04-14 11:19:47 +0530

    Added camera and fuel instantiate (tank fuel game)

---
+++ Assets/Scripts/TankGame/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform tank;
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(tank.position.x, tank.position.y, this.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/TankGame/FuelInstantiate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelInstantiate : MonoBehaviour
+{
+    public GameObject fuel;
+    void Start()
+    {
+        GameObject fuelObj = Instantiate(fuel, new Vector3(Random.Range(-100, 100), Random.Range(-100, 100), this.gameObject.transform.position.z), Quaternion.identity);
+        Debug.Log("Fuel location: " + fuelObj.transform.position);
+    }
+}
